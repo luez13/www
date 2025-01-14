735f83bbad7de0e32a91686d5ce64ab00ab3ec3e
luez13 <109839749+luez13@users.noreply.github.com>
2025-01-13 23:45:09 -0400

    fuente del certificado

---
+++ certificaciones/public/assets/vendor/edwardianscriptitc-normal.js
@@ -0,0 +1,7 @@
+﻿import { jsPDF } from "jspdf"
+var font = 'undefined';
+var callAddFont = function () {
+this.addFileToVFS('edwardianscriptitc-normal.ttf', font);
+this.addFont('edwardianscriptitc-normal.ttf', 'edwardianscriptitc', 'normal');
+};
+jsPDF.API.events.push(['addFonts', callAddFont])
